5689291f9cf051fab34df15b5946a4f12e1da197
absinth84 <absinth84@gmail.com>
2016-10-25 01:21:47 +0200

    Fix graph

---
+++ include/header3.php
@@ -0,0 +1,18 @@
+<head>
+    <title>Raspberry Py</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="../css/main.css" />
+</head>
+<body class="subpage">
+
+
+<header id="header">
+    <div class="inner">
+        <a href="../home/index.php" class="logo">Raspberry Py</a>
+        <nav id="nav">
+            <a href="../home/index.php">Home</a>
+            <a href="../termo/termo.php">Monitor</a>
+        </nav>
+        <a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+    </div>
--- termo/termo3.php
+++ termo/termo3.php
@@ -1,13 +1,18 @@
 <html>
 
+<link href='../css/main.css' rel='stylesheet' type='text/css' />
 
 <script type="text/javascript" src="../lib/Chart.bundle.js"></script>
-<canvas id="myChart" width="200" height="200"></canvas>
+
+<?php include("../include/header3.php")?>
+<
+</header>
+
+<canvas id="myChart" hight="200", width="300"></canvas>
+
+
 
 <script language=javascript>
-
-var  datarelay = [], dataest = [], options;
-
 
 <?php
 $start=-300;
@@ -20,87 +25,120 @@ $redis->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_NONE);
 
 $lettura      = $redis->lRange('lettura', $start, $end);
 $timestamp    = $redis->lRange('timestamp', $start, $end);
-$temp_esterna = $redis->lRange('temp_esterna', $start, $end);
+$tempext 	  = $redis->lRange('temp_esterna', $start, $end);
 $termo        = $redis->lRange('camera', $start, $end);
-$rele         = $redis->lRange('rele', $start, $end);
+$relay         = $redis->lRange('rele', $start, $end);
 $min	      = $redis->lRange('min', $start, $end);
 $max	      = $redis->lRange('max', $start, $end);
 
-$data = array
-	(
-		array(),
-		array()
-	);
 
 for ($i=0;$i<=(abs($start)-1);$i++) {
+	$data_temp[$i][x] =  $timestamp[$i];
+	$data_temp[$i][y] =  $termo[$i];
 
-	//		datarelay.push([" . $timestamp[$x] .", ". ($rele[$x] * 10) . "]);\n";
-	//		dataest.push([" . $timestamp[$x] .", ". $temp_esterna[$x] . "]);\n";
+	$data_tempext[$i][x] =  $timestamp[$i];
+	$data_tempext[$i][y] =  $tempext[$i];
 
-	//	$data .= '{x: ' . $temp_esterna[$i] . ', y: ' . $termo[$i] . '},';
-//	array_push[0]($data, "x: " . $timestamp[$i] . ", y: " . $termo[$i]);
+	$data_relay[$i][x] =  $timestamp[$i];
+	$data_relay[$i][y] =  $relay[$i] * 10;
 
-	$data[$i][x] =  $timestamp[$i];
-	$data[$i][y] =  $termo[$i];
 }
 
+
 ?>
-
-
-//alert(<?php echo json_encode(array_values($data)); ?>);
-
-
-$json = <?php echo json_encode($data); ?>;
-
-
-var serie = [{ x: 19, y: 65 },{ x: 27, y: 59 },{ x: 28, y: 69 },{ x: 40, y: 81 },{ x: 48, y: 56 }];
 
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
 	type: 'line',
 	data: {
 		datasets: [{
-			label: 'Scatter Dataset',
-			data: <?php echo json_encode($data); ?>
-		}]
+			label: 'Temp internal',
+			fill: false,
+			borderColor: "rgba(75,192,192,1)",
+			borderCapStyle: 'butt',
+			pointBorderColor: "rgba(75,192,192,1)",
+			pointBackgroundColor: "#fff",
+			pointBorderWidth: 1,
+			pointHoverRadius: 5,
+			pointHoverBackgroundColor: "rgba(75,192,192,1)",
+			pointHoverBorderColor: "rgba(220,220,220,1)",
+			pointHoverBorderWidth: 2,
+			pointRadius: 1,
+			pointHitRadius: 10,
+			data: <?php echo json_encode($data_temp);?>
+		},
+		{
+			label: 'Temp external',
+			fill: true,
+			backgroundColor: "rgba(38,45,255,0.3)",
+			borderColor: "blue",
+			borderCapStyle: 'butt',
+			pointBorderColor: "blue",
+			pointBackgroundColor: "#fff",
+			pointBorderWidth: 1,
+			pointHoverRadius: 5,
+			pointHoverBackgroundColor: "rgba(75,192,192,1)",
+			pointHoverBorderColor: "rgba(220,220,220,1)",
+			pointHoverBorderWidth: 2,
+			pointRadius: 1,
+			pointHitRadius: 10,
+			data: <?php echo json_encode($data_tempext);?>
+			},
+			{
+				label: 'Relay',
+				fill: true,
+				backgroundColor: "rgba(255,10,10,0.6)",
+				borderColor: "red",
+				borderCapStyle: 'butt',
+				pointBorderColor: "red",
+				pointBackgroundColor: "#fff",
+				pointBorderWidth: 1,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor: "rgba(75,192,192,1)",
+				pointHoverBorderColor: "rgba(220,220,220,1)",
+				pointHoverBorderWidth: 2,
+				pointRadius: 1,
+				pointHitRadius: 10,
+				data: <?php echo json_encode($data_relay);?>
+		}],
+
 	},
 	options: {
-				responsive: true,
-                title:{
-                    display:true,
-                    text:"Chart.js Time Point Data"
-                },
-				scales: {
-					xAxes: [{
-						type: 'time',
-						time: {
-							format: 'X',
+		responsive: true,
+		title:{
+			display:true,
+			text:"Temperatur"
+		},
+		scales: {
+			xAxes: [{
+				type: 'time',
+				time: {
+					format: 'X',
 					//		 round: 'day',
-						},
+				},
 
-						display: true,
-						scaleLabel: {
-							display: true,
-							labelString: 'Date'
-						}
-					}],
-					yAxes: [{
-						type: 'linear',
-						display: true,
-						scaleLabel: {
-							display: true,
-							labelString: 'Temp'
-						}
-					}]
+				display: true,
+				scaleLabel: {
+					display: true,
+					labelString: 'Date'
 				}
-			}
+			}],
+			yAxes: [{
+				type: 'linear',
+				display: true,
+				position: "right",
+				scaleLabel: {
+					display: true,
+					labelString: 'Temp'
+				}
+			}]
+		}
+	}
 });
 
 
 </script>
 
-
-</head>
 
 <body>
 </body>
